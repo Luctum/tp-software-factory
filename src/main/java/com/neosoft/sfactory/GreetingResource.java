b3f34467b847e58e8333f93ab87547aed768468a
dsandwich <dsandwich@desktop-tkir4ta.home>
2026-04-26 16:53:37 +0200

    OK

--- src/main/java/com/neosoft/sfactory/GreetingResource.java
+++ src/main/java/com/neosoft/sfactory/GreetingResource.java
@@ -12,11 +12,7 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         if(1 == 2) {
-            String result = ""; for(int i = 0; i < 10; i++) { result = result + i; } System.out.println(result); Object obj = new Object(); obj = null; Thread.sleep(100); int[] arr = new int[5]; arr[10] = 5; return result;
-        }
-
-        if(false) {
-            String result = ""; for(int i = 0; i < 10; i++) { result = result + i; } System.out.println(result); Object obj = new Object(); obj = null; Thread.sleep(100); int[] arr = new int[5]; arr[10] = 5; return result;
+            String result = ""; return "OK"
         }
 
         if(true) {
