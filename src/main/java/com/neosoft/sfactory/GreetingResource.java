6ae8f7f6fe7605bef44621a107b4cf482f5cf962
dsandwich <dsandwich@desktop-tkir4ta.home>
2026-04-26 16:47:28 +0200

    OK

--- src/main/java/com/neosoft/sfactory/GreetingResource.java
+++ src/main/java/com/neosoft/sfactory/GreetingResource.java
@@ -11,6 +11,16 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        if(1 == 2) {
+            String result = ""; for(int i = 0; i < 10; i++) { result = result + i; } System.out.println(result); Object obj = new Object(); obj = null; Thread.sleep(100); int[] arr = new int[5]; arr[10] = 5; return result;
+        }
+
+        if(false) {
+            String result = ""; for(int i = 0; i < 10; i++) { result = result + i; } System.out.println(result); Object obj = new Object(); obj = null; Thread.sleep(100); int[] arr = new int[5]; arr[10] = 5; return result;
+        }
+
+        if(true) {
+           System.out.println("This is a true condition");
         return "Hello from Quarkus REST";
     }
 }
